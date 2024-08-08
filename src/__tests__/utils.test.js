868e9bcc7dae7c8284899813fb50acafa675dc93
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-08 21:21:12 +0300

    Writing tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,20 @@
 // Your tests here
+import {pointsForWord} from '../utils'
+
+describe("pointsForWord", () => {
+    it("calculates the total numbers of points for a word(1 point for a vowel and 2 points for a consonant)", () => {
+        const word = "test";
+
+        const points = pointsForWord(word);
+    
+        expect(points).toBe(7);
+      });
+
+      it("handles uppercase and lowercase input", () => {
+        const word = "tEsT";
+      
+        const points = pointsForWord(word);
+      
+        expect(points).toBe(7);
+      });
+})
